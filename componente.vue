87b1e3d9cf91aa16c32685340fd2aa073502ac03
jorivaalvgar <jorivaalv@hotmail.com>
2019-05-21 21:15:36 -0500

    Vue interface
    Vue interface

---
+++ componente.vue
@@ -0,0 +1,15 @@
+var  vm = new Vue({
+   el: '#test',
+   data: {
+      mensaje: 'Ingrese el sitio a explorar',
+      mensaje2: '',
+      sitio: ''
+   },
+   methods: {
+       enviar: function(event){
+           return this.mensaje2 = this.sitio;
+       }
+    },
+   computed: {
+     }
+})
